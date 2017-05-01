ea6041b343e3a2feb0fb4d5b2a45b951f65e7a6b
autopp <autopp.inc@gmail.com>
2017-05-01 22:58:30 +0900

    impl normal case of Options.parse

--- lib/ruam/options.rb
+++ lib/ruam/options.rb
@@ -5,6 +5,23 @@ module Ruam
   #
   class Options
     def self.parse(argv)
+      options = { compile_options: {} }
+      parser = create_parser(options[:compile_options])
+      file = parser.parse(argv).first
+      options[:file] = file
+      options
+    end
+
+    def self.create_parser(options)
+      parser = OptionParser.new
+      Ruam.optimization_flags.each do |flag|
+        parser.on("--[no-]#{flag.to_s.tr('_', '-')}") do |v|
+          options[flag] = v
+        end
+      end
+      parser.on('--debug-level=LEVEL') do |v|
+        options[:debug_level] = v.to_i
+      end
     end
   end
 end
