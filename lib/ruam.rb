e5aaae3cec9a5c195e708e1800d9bd74d6fdfa7c
autopp <autopp.inc@gmail.com>
2017-05-02 16:31:58 +0900

    impl Ruam.run

--- lib/ruam.rb
+++ lib/ruam.rb
@@ -6,6 +6,14 @@ module Ruam
   def self.optimization_flags
     RubyVM::InstructionSequence.compile_option.keys - %i[debug_level]
   end
+
+  def self.run(argv)
+    options = Options.parse(argv)
+    file = options[:file]
+    compile_options = RubyVM::InstructionSequence.compile_option.merge(options[:compile_options])
+    insns = RubyVM::InstructionSequence.compile_file(file, compile_options)
+    puts insns.disasm
+  end
 end
 
 require 'ruam/options'
--- spec/ruam_spec.rb
+++ spec/ruam_spec.rb
@@ -3,11 +3,23 @@ require 'spec_helper'
 describe Ruam do
   describe '.optimization_flags' do
     subject { described_class.optimization_flags }
+
     it 'returns strings according to names defined in the VM' do
       options = RubyVM::InstructionSequence.compile_option.keys
       expected = (options - %i[debug_level]).sort
 
       expect(subject.sort).to eq(expected)
+    end
+  end
+
+  describe '.run' do
+    subject { described_class.run(%W[#{__FILE__} --peephole-optimization]) }
+
+    it 'compile given file and output the byte code' do
+      insns = RubyVM::InstructionSequence.compile_file(__FILE__, peephole_optimization: true)
+
+      expect(RubyVM::InstructionSequence).to receive(:compile_file).and_call_original
+      expect { subject }.to output(insns.disasm).to_stdout
     end
   end
 end
