441851b3d4a006c308045632297da0cd039a5d42
autopp <autopp.inc@gmail.com>
2017-05-12 08:38:54 +0900

    add test for compiling stdin

--- spec/ruam_spec.rb
+++ spec/ruam_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'tempfile'
+require 'stringio'
 
 describe Ruam do
   describe '.optimization_flags' do
@@ -29,8 +30,24 @@ describe Ruam do
 
     context 'without file name' do
       let(:argv) { [] }
+      let(:code) { 'puts "hello world"' }
 
-      it 'compile given stdin and output the byte code'
+      before do
+        @orig_stdin = $stdin
+        $stdin = StringIO.new('puts "hello world"')
+      end
+
+      it 'compile given stdin and output the byte code' do
+        insns = RubyVM::InstructionSequence.compile(code)
+
+        expect(RubyVM::InstructionSequence).to receive(:compile).and_call_original
+        expect { subject }.to output(insns.disasm).to_stdout
+      end
+
+      after do
+        $stdin.close
+        $stdin = @orig_stdin
+      end
     end
 
     context 'with unknown option' do
